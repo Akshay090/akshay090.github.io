c5d773751111f5fa9a0f798d40f0d7bfd9791b44
Akshay Ashok <aks28id@gmail.com>
2020-08-12 01:30:45 +0530

    post card added

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,4 +1,4 @@
-import codeTheme from "@theme-ui/prism/presets/night-owl-light.json";
+import codeTheme from "@theme-ui/prism/presets/dracula.json";
 import baseTheme from "@pauliescanlon/gatsby-theme-terminal/src/gatsby-plugin-theme-ui";
 
 export default {
@@ -11,20 +11,70 @@ export default {
     secondary: "#c39eff",
     error: "#ff4343",
     success: "#58e6d9",
-    background: "#131127",
-    surface: "#232140",
+    background: "#1d2224",
+    surface: "#2d3831",
   },
+  
   styles: {
     ...baseTheme.styles,
     p: {
+      a: {
+        ...baseTheme.styles.p.a,
+        color: "secondary",
+        wordBreak: "break-word",
+      },
       code: {
         ...baseTheme.styles.p.code,
-        color: "inherit"
-      }
+        color: "inherit",
+        fontSize: "14px",
+        wordBreak: "break-word",
+        backgroundColor: "surface",
+      },
     },
     pre: {
       ...baseTheme.styles.pre,
-      ...codeTheme
-    }
-  }
-};
+      ...codeTheme,
+    },
+    a: {
+      ...baseTheme.styles.a,
+      color: "secondary",
+      button: {
+        cursor: "pointer",
+      },
+    },
+  },
+
+  buttons: {
+    ...baseTheme.buttons,
+    success: {
+      ...baseTheme.buttons.success,
+    },
+    primary: {
+      ...baseTheme.buttons.primary,
+      ":disabled": {
+        cursor: "not-allowed",
+        backgroundColor: "surface",
+      },
+    },
+    ghost: {
+      ...baseTheme.buttons.primary,
+      color: "muted",
+      backgroundColor: "background",
+      transition: ".2s linear background-color",
+      ":hover": {
+        backgroundColor: "surface",
+      },
+    },
+  },
+
+  cards: {
+    primary: {
+      ...baseTheme.cards.primary,
+      transition: ".2s linear box-shadow, .2s ease-in-out transform",
+      ":hover": {
+        transform: "translateY(-0.25rem)",
+        boxShadow: 2,
+      },
+    },
+  },
+}
