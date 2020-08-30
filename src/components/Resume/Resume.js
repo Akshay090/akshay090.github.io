337a85dc4379e2b751a1d1d088420f85e8e9f04e
Akshay Ashok <aks28id@gmail.com>
2020-08-31 02:40:33 +0530

    resume pdf added

---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { Document, Page } from "react-pdf";
+
+import { pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+
+export const Resume = ({file}) => {
+  const [numPages, setNumPages] = useState(null);
+ 
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  return (
+    <div>
+      <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
+      {Array.from(
+        new Array(numPages),
+        (el, index) => (
+          <Page
+            key={`page_${index + 1}`}
+            pageNumber={index + 1}
+          />
+        ),
+      )}
+      </Document>
+    
+    </div>
+  );
+};
